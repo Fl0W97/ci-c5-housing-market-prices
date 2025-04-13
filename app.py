e407ba6f16db647b737d971b8bb185f7341ed95d
FlorianG <46726822+Fl0W97@users.noreply.github.com>
2025-04-13 16:56:08 +0000

    add page_pipeline_performance, just the file

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from app_pages.page_project_summary import page_project_summary_body
 from app_pages.page_house_market_study import page_house_market_study_body
 from app_pages.page_hypothesis_and_validation import page_project_hypothesis_and_validation_body
 from app_pages.page_sales_price_predictor import page_sales_price_predictor_body
+from app_pages.page_pipeline_performance import page_pipeline_performance_body
 
 app = MultiPage(app_name= "House Market Analysis (Project 5)") # Create an instance of the app
 
@@ -19,5 +20,6 @@ app.add_page("Quick Project Summary", page_project_summary_body)
 app.add_page("House Market Study", page_house_market_study_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_and_validation_body)
 app.add_page("Sales Price Predictor", page_sales_price_predictor_body)
+app.add_page("ML: Pipeline Performance", page_pipeline_performance_body)
 
 app.run() # Run the  app
---
+++ app_pages/page_pipeline_performance.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+from src.data_management import load_pkl_file
+
+def page_pipeline_performance_body():
+     st.write("### Pipeline performance")
+
+    #
