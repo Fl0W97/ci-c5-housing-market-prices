c39a1e3f3402cb4bea482b04cd3f1cb0eea0bc34
FlorianG <46726822+Fl0W97@users.noreply.github.com>
2025-04-13 11:16:19 +0000

    add/adjust page_project_summary (draft)

--- app_pages/page_project_summary.py
+++ app_pages/page_project_summary.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def page_summary_body():
+def page_project_summary_body()():
 
     st.write("### Quick Summary of Project 5")
 
@@ -20,7 +20,7 @@ def page_summary_body():
     st.success(
         f"The project has 2 business requirements:\n"
         f"* 1 - The client is interested in discovering how the house attributes correlate with the sale price. "
-        f" Therefore, the client expects data visualisations of the correlated variables against the sale price to show that. "
+        f" Therefore, the client expects data visualisations of the correlated variables against the sale price to show that. \n"
         f"* 2 - The client is interested in predicting the house sale price from her four inherited houses "
         f"and any other house in Ames, Iowa "
         )
